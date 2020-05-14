e17b6331f823ded6361cc142ae1cd5695dc0d7c1
Abdo Tareq <abdotarekfathy@gmail.com>
2020-05-14 21:48:29 +0200

    fix: remove cashing make proplems if updated

--- app/src/main/java/com/simplute/android/cartaskandroid/ui/BindingItem.kt
+++ app/src/main/java/com/simplute/android/cartaskandroid/ui/BindingItem.kt
@@ -4,6 +4,7 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.simplute.android.cartaskandroid.R
 
@@ -17,7 +18,9 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
                 RequestOptions()
                 .placeholder(R.drawable.loading_animation)
                 .override(100, 100) // resize does not respect aspect ratio
-
+                    // for changing profile picture to work
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .skipMemoryCache(true)
             )
             .into(imgView)
     }
